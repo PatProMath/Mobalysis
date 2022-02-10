34708ea63c95a484d3f4a714f59516bcdfe07f47
Mathew Kiruri <56315538+Keyruri@users.noreply.github.com>
2022-02-10 15:44:34 +0300

    Provisional-1

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-#To install PostgreSQL on Ubuntu 20.04
-# Run Ubuntu 20.04 system update 
-sudo apt update
-#Add PostgreSQL GPG key & repository
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
-#Add repository in Ubuntu 20.04
-echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
-# Run system update command
-sudo apt update
-sudo apt install postgresql-13
-sudo systemctl status postgresql
-# Add user
-sudo useradd mob_db_user
-sudo passwd mob_db_user
-# add OS user
-sudo useradd -m mob_app_usr
-# check for initialization files
-ls -la /home/mob_app_user/
+# Initial
+   sudo apt update
+   sudo apt install postgresql postgresql-contrib
+#Verify the Install   the numuber 12 depicts the version of postgres change it accordingly
+   ls /etc/postgresql/12/main/
+# Check status of the postgresql if active the database is running
+    service postgresql status
+
+# SET ROOT USER CREDENTIALS POSTGRESQL
+###############################
+# Login to PostgreSQL interactive shell
+sudo -u postgres psql
+
+# Set the root user credentials
+ALTER USER postgres PASSWORD 'newpassword';
+
+# Login to psql
+psql -U postgres -h localhost 
+
+#  log in to the PostgreSQL server
+sudo su postgres
+
+# create a new user
+CREATE USER mob_db_user WITH PASSWORD ' mob_db_pass';
+
